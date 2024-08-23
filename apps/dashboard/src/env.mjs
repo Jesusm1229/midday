e75a6f0c1ae4c706ee1a07ea81c4b77b2421a928
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2024-08-22 20:57:03 -0400

    packages changes

--- apps/dashboard/src/env.mjs
+++ apps/dashboard/src/env.mjs
@@ -13,23 +13,8 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app isn't
    * built with invalid env vars.
    */
-  server: {
-    PLAIN_API_KEY: z.string(),
-    OPENAI_API_KEY: z.string(),
+  server: {   
     SUPABASE_SERVICE_KEY: z.string(),
-    UPSTASH_REDIS_REST_TOKEN: z.string(),
-    UPSTASH_REDIS_REST_URL: z.string(),
-    LOOPS_ENDPOINT: z.string(),
-    LOOPS_API_KEY: z.string(),
-    GOCARDLESS_SECRET_ID: z.string(),
-    GOCARDLESS_SECRET_KEY: z.string(),
-    NOVU_API_KEY: z.string(),
-    RESEND_API_KEY: z.string(),
-    OPENPANEL_SECRET_KEY: z.string(),
-    MIDDAY_ENGINE_API_KEY: z.string(),
-    WEBHOOK_SECRET_KEY: z.string(),
-    AZURE_DOCUMENT_INTELLIGENCE_ENDPOINT: z.string(),
-    AZURE_DOCUMENT_INTELLIGENCE_KEY: z.string(),
   },
   /**
    * Specify your client-side environment variables schema here.
@@ -38,13 +23,7 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
-    NEXT_PUBLIC_NOVU_APPLICATION_IDENTIFIER: z.string(),
     NEXT_PUBLIC_SUPABASE_ID: z.string(),
-    NEXT_PUBLIC_TRIGGER_API_KEY: z.string(),
-    NEXT_PUBLIC_TELLER_APPLICATION_ID: z.string(),
-    NEXT_PUBLIC_TELLER_ENVIRONMENT: z.string(),
-    NEXT_PUBLIC_PLAID_ENVIRONMENT: z.string(),
-    NEXT_PUBLIC_OPENPANEL_CLIENT_ID: z.string(),
   },
   /**
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
